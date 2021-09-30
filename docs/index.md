9e0f4f7c768d2b8343fca6f9221ca1c6e32924b0
Shabie Iqbal <shabieiqbal@gmail.com>
2021-09-30 22:07:39 +0200

    clean up and go!

---
+++ docs/index.md
@@ -0,0 +1,31 @@
+# DocFormer - PyTorch
+
+![docformer architecture](images/docformer-architecture.png)
+
+Implementation of [DocFormer: End-to-End Transformer for Document Understanding](https://arxiv.org/abs/2106.11539), a multi-modal transformer based architecture for the task of Visual Document Understanding (VDU) 📄📄📄.
+
+The official implementation was not released by the authors.
+
+## Install
+
+
+## Usage
+
+See `examples` for usage.
+
+
+## Contribute
+
+
+## Citations
+
+```bibtex
+@misc{appalaraju2021docformer,
+    title   = {DocFormer: End-to-End Transformer for Document Understanding},
+    author  = {Srikar Appalaraju and Bhavan Jasani and Bhargava Urala Kota and Yusheng Xie and R. Manmatha},
+    year    = {2021},
+    eprint  = {2106.11539},
+    archivePrefix = {arXiv},
+    primaryClass = {cs.CV}
+}
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'docformer',
+  packages = find_packages(exclude=['examples']),
+  version = '0.0.1',
+  license='MIT',
+  description = 'DocFormer: End-to-End Transformer for Document Understanding',
+  author = 'Akarsh Upadhay, Shabie Iqbal',
+  author_email = '[add email], shabieiqbal@gmail.com',
+  url = 'https://github.com/shabie/docformer',
+  keywords = [
+    'artificial intelligence',
+    'attention mechanism',
+    'document understanding',
+  ],
+  install_requires=[
+    'einops>=0.3',
+    'torch>=1.6',
+    'torchvision'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
