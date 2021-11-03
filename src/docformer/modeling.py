5a8d1a6f43bfada3243c060020ae046df0afafe1
uakarsh <55104596+uakarsh@users.noreply.github.com>
2021-11-03 10:51:46 +0530

    Adding the MLM+IR Model of DocFormer

--- src/docformer/modeling.py
+++ src/docformer/modeling.py
@@ -520,3 +520,62 @@ class DocFormerForClassification(nn.Module):
         output = self.classifier(output)
         return output
 
+    
+    
+## Adding the code for the MLM + Image Reconstruction
+# The Shallow decoder can be modified as per the requirement
+
+class ShallowDecoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # decoder 
+        self.linear1 = nn.Linear(in_features = 768,out_features = 512)                        # Making the image to be symmetric
+        self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 3,kernel_size = 3,stride = 1)
+        self.conv2 = nn.Conv2d(in_channels = 3, out_channels = 3,kernel_size = 3,stride = 1)
+
+        self.conv3 = nn.Conv2d(in_channels = 3, out_channels = 3,kernel_size = 5,stride = 1)
+        self.conv4 = nn.Conv2d(in_channels = 3, out_channels = 3,kernel_size = 5,stride = 2)
+
+        self.conv5 = nn.Conv2d(in_channels = 3, out_channels = 3,kernel_size = 7)
+        self.conv6 = nn.Conv2d(in_channels = 3, out_channels = 3,kernel_size = 7)
+
+        self.conv7 = nn.Conv2d(in_channels = 3, out_channels = 3,kernel_size = 7)
+        self.conv8 = nn.Conv2d(in_channels = 3, out_channels = 3,kernel_size = 7)
+
+        self.conv9 = nn.Conv2d(in_channels = 3, out_channels = 3,kernel_size = 3,stride = 1)
+
+    def forward(self, x):
+          x = x.unsqueeze(1).cuda()
+          x = nn.ReLU()(torch.nn.BatchNorm2d(num_features=1).cuda()(self.linear1(x)))
+          x = nn.ReLU()(torch.nn.BatchNorm2d(num_features=3).cuda()(self.conv1(x)))
+          x = nn.ReLU()(torch.nn.BatchNorm2d(num_features=3).cuda()(self.conv2(x)))
+          x = nn.ReLU()(torch.nn.BatchNorm2d(num_features=3).cuda()(self.conv3(x)))
+          x = nn.ReLU()(torch.nn.BatchNorm2d(num_features=3).cuda()(self.conv4(x)))
+          x = nn.ReLU()(torch.nn.BatchNorm2d(num_features=3).cuda()(self.conv5(x)))
+          x = nn.ReLU()(torch.nn.BatchNorm2d(num_features=3).cuda()(self.conv6(x)))
+          x = nn.ReLU()(torch.nn.BatchNorm2d(num_features=3).cuda()(self.conv7(x)))
+          x = nn.ReLU()(torch.nn.BatchNorm2d(num_features=3).cuda()(self.conv8(x)))
+          x = nn.ReLU()(torch.nn.BatchNorm2d(num_features=3).cuda()(self.conv9(x)))
+          return torch.sigmoid(x)
+
+class DocFormer_For_IR(nn.Module):
+    def __init__(self, config,num_classes= 30522):
+        super().__init__()
+        self.config = config
+        self.extract_feature = ExtractFeatures(config)
+        self.encoder = DocFormerEncoder(config)
+        self.dropout = nn.Dropout(config['hidden_dropout_prob'])
+        self.classifier = nn.Linear(in_features = 768,out_features = num_classes)
+        self.decoder = ShallowDecoder().cuda()
+
+    def forward(self, x):
+        v_bar, t_bar, v_bar_s, t_bar_s = self.extract_feature(x)
+        features = {'v_bar': v_bar, 't_bar': t_bar, 'v_bar_s': v_bar_s, 't_bar_s': t_bar_s}
+        for f in features:
+            features[f] = features[f]
+        output = self.encoder(features['t_bar'], features['v_bar'], features['t_bar_s'], features['v_bar_s'])
+        output = self.dropout(output)
+        output_mlm = self.classifier(output)
+        output_ir = self.decoder(output) 
+        return {'mlm_labels':output_mlm,'ir':output_ir}
